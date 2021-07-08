e4ec923646aa93c5064e78936252339e62a66a68
Stefan Sonski <s.sonski@gmail.com>
2021-07-08 07:27:22 +0200

    Show treesitter status only for current window
    nvim_treesitter#statusline() currently supports only the active window.

--- powerline_treesitter/segments.py
+++ powerline_treesitter/segments.py
@@ -7,11 +7,16 @@ try:
 except ImportError:
     vim = object()
 
+from powerline.theme import requires_segment_info
 
-def treesitter(pl):
+@requires_segment_info
+def treesitter(pl, segment_info):
     '''Return treesitter status for the current position.
     Highlight groups used: ``treesitter_identifier`` or ``file_name``.
     '''
+    if vim.current.window.number != segment_info['window'].number:
+        return None
+
     winwidth = vim.eval('nvim_win_get_width(0)')
     treesitter_status = vim.eval('nvim_treesitter#statusline(' +
                                  winwidth + ')')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(
     name='powerline-treesitter',
     description='A Powerline segment for showing treesitter status.',
-    version='0.1.1',
+    version='0.2.0',
     keywords='powerline treesitter nvim neovim prompt',
     author='Stefan Sonski',
     author_email='s.sonski@gmail.com',
